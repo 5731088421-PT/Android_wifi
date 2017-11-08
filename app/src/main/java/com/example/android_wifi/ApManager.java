1ce6723deeb52142badd49eaceb5131bfbc07478
Panupong <panupong.not@hotmail.com>
2017-11-08 18:23:50 +0700

    Add missing file

--- app/src/main/java/com/example/android_wifi/ApManager.java
+++ app/src/main/java/com/example/android_wifi/ApManager.java
@@ -9,10 +9,10 @@ import android.os.Build;
 import android.os.Handler;
 import android.support.annotation.RequiresApi;
 import android.util.Log;
-
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.List;
-
+import java.lang.reflect.Proxy;
 import static android.content.ContentValues.TAG;
 
 /**
@@ -37,44 +37,106 @@ public class ApManager {
         return false;
     }
 
-    public static void  startTethering(Context context) throws ClassNotFoundException, NoSuchMethodException {
+    public static void  startTethering(Context context) {
         ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        Class callback = Class.forName("ConnectivityManager.OnStartTetheringCallback");
-        Method m = connectivityManager.getClass().getMethod("a", new Class[]{int.class, boolean.class, callback});
+        try {
+            Method [] ms = connectivityManager.getClass().getMethods();
+            Method targetM = null;
+            for (Method m : ms) {
+                if (m.getName() == "startTethering" && m.getParameterTypes().length == 3) {
+                    targetM = m;
+                }
+            }
+            if (targetM == null) {
+                throw new Exception("Exception");
+            }
+
+            Class[] cs = connectivityManager.getClass().getDeclaredClasses();
+            Class c = null;
+            for (Class cc: cs) {
+                if (cc.getName().equals("android.net.ConnectivityManager$OnStartTetheringCallback")) {
+                    c = cc;
+                }
+            }
+
+            if (c == null) {
+                throw new Exception("Exception");
+            }
+
+            Object o = Proxy.newProxyInstance(c.getClassLoader(), new Class[]{}, new InvocationHandler() {
+                @Override
+                public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                    if (method.getName() == "onTetheringStarted" ){
+
+                        Log.d("apManager", "Start");
+                    }
+                    return null;
+                }
+            });
+
+            targetM.invoke(connectivityManager,0,true,o);
+//            m.invoke(connectivityManager,new Object[]{0,true, new Object() {
+//                public void onTetheringStarted() {
+//                    Log.d("apManager", "Start");
+//                }
+//                public void onTetheringFailed() {
+//                    Log.d("apManager", "Failed");
+//                }
+//            }});
+        }
+        catch (Exception e){
+            Log.d(TAG, "Exce");
+        }
 
     }
 
     // toggle wifi hotspot on or off
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    public static boolean configApState(final Context context) {
+//    @RequiresApi(api = Build.VERSION_CODES.O) c
+    public boolean configApState(final Context context, final MainActivity activity) {
 
-        final WifiManager wifimanager = (WifiManager) context.getSystemService(context.WIFI_SERVICE);
-        wifimanager.startLocalOnlyHotspot(new LocalOnlyHotspotCallback() {
-            @Override
-            public void onStarted(WifiManager.LocalOnlyHotspotReservation reservation) {
-                super.onStarted(reservation);
-                Log.d(TAG, "Wifi Hotspot is on now");
-                WifiConfiguration config = reservation.getWifiConfiguration();
-                Log.d(TAG, isHotspotOn(context) ? "hotspot is on" : "hot spot is off");
-                config.SSID = "TEST";
-                wifimanager.updateNetwork(config);
-            }
-
-            @Override
-            public void onStopped() {
-                super.onStopped();
-                Log.d(TAG, "onStopped: ");
-            }
-
-            @Override
-            public void onFailed(int reason) {
-                super.onFailed(reason);
-                Log.d(TAG, "onFailed: ");
-            }
-        } ,new Handler());
-
-
-
+//        final WifiManager wifimanager = (WifiManager) context.getSystemService(context.WIFI_SERVICE);
+//        wifimanager.startLocalOnlyHotspot(new LocalOnlyHotspotCallback() {
+//            @Override
+//            public void onStarted(WifiManager.LocalOnlyHotspotReservation reservation) {
+//                super.onStarted(reservation);
+//                Log.d(TAG, "Wifi Hotspot is on now");
+//                Log.d(TAG, isHotspotOn(context) ? "hotspot is on" : "hot spot is off");
+//                WifiConfiguration config = reservation.getWifiConfiguration();
+//                config.SSID = "1234";
+//                Method setConfigMethod = null;
+//                try {
+//                    setConfigMethod = wifimanager.getClass().getMethod("setWifiApConfiguration", WifiConfiguration.class);
+//                } catch (NoSuchMethodException e) {
+//                    e.printStackTrace();
+//                }
+//                try {
+//                    setConfigMethod.invoke(wifimanager, config);
+//                } catch (IllegalAccessException e) {
+//                    e.printStackTrace();
+//                } catch (InvocationTargetException e) {
+//                    e.printStackTrace();
+//                }
+//                config.SSID = "TEST";
+//                wifimanager.updateNetwork(config);
+//                activity.setHotSpotNameWrap("HELLO",context);
+//            }
+//
+//            @Override
+//            public void onStopped() {
+//                super.onStopped();
+//                Log.d(TAG, "onStopped: ");
+//            }
+//
+//            @Override
+//            public void onFailed(int reason) {
+//                super.onFailed(reason);
+//                Log.d(TAG, "onFailed: ");
+//            }
+//        } ,new Handler());
+//
+//
+//
+//
 //        WifiConfiguration wificonfiguration = null;
 //        try {
 //            // if WiFi is on, turn it off
@@ -88,8 +150,28 @@ public class ApManager {
 //        catch (Exception e) {
 //            e.printStackTrace();
 //        }
+//
+
         return false;
     }
 
+    public static boolean setHotspotName(String newName, Context context) {
+        try {
+            WifiManager wifiManager = (WifiManager) context.getSystemService(context.WIFI_SERVICE);
+            Method getConfigMethod = wifiManager.getClass().getMethod("getWifiApConfiguration");
+            WifiConfiguration wifiConfig = (WifiConfiguration) getConfigMethod.invoke(wifiManager);
+
+            wifiConfig.SSID = newName;
+
+            Method setConfigMethod = wifiManager.getClass().getMethod("setWifiApConfiguration", WifiConfiguration.class);
+            setConfigMethod.invoke(wifiManager, wifiConfig);
+
+            return true;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 
 }
--- app/src/main/java/com/example/android_wifi/MainActivity.java
+++ app/src/main/java/com/example/android_wifi/MainActivity.java
@@ -1,29 +1,40 @@
 package com.example.android_wifi;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.lang.reflect.Method;
 import java.sql.Time;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import static android.content.ContentValues.TAG;
+
 public class MainActivity extends AppCompatActivity {
 
     WifiManager wifiManager;
-    Timer timer;
-    TimerTask timerTask;
+//    Timer timer;
+//    TimerTask timerTask;
     Context context;
-    int time = 0;
+//    int time = 0;
+    final ApManager apManager = new ApManager();
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
@@ -32,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         context = getApplicationContext();
-        ApManager.startTethering(context);
+//        ApManager.startTethering(context);
 //        wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
 
 //        context.registerReceiver(new BroadcastReceiver() {
@@ -53,14 +64,92 @@ public class MainActivity extends AppCompatActivity {
 //        timer.scheduleAtFixedRate(timerTask,0,5000);
 
 
-        ApManager apManager = new ApManager();
+
         if (!apManager.isHotspotOn(context)){
-            apManager.configApState(context);
+            apManager.configApState(context, this);
         }
 
 //        Log.d("stat", printWifi());
     }
 
+    private final int REQUEST_CODE_ASK_PERMISSIONS = 123;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void setHotSpotNameWrap(String name , Context context){
+
+        int permissionCheck = ContextCompat.checkSelfPermission(
+                this, "android.permission.OVERRIDE_WIFI_CONFIG");
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    "android.permission.OVERRIDE_WIFI_CONFIG")) {
+                showExplanation("Permission Needed", "Rationale", "android.permission.OVERRIDE_WIFI_CONFIG", REQUEST_CODE_ASK_PERMISSIONS);
+            } else {
+                requestPermission("android.permission.OVERRIDE_WIFI_CONFIG", REQUEST_CODE_ASK_PERMISSIONS);
+            }
+        } else {
+            Toast.makeText(MainActivity.this, "Permission (already) Granted!", Toast.LENGTH_SHORT).show();
+            apManager.setHotspotName(name,context);
+        }
+
+
+
+
+//        int hasChangeConfigPermission = ContextCompat.checkSelfPermission(context,"android.permission.OVERRIDE_WIFI_CONFIG");
+//        if(hasChangeConfigPermission != PackageManager.PERMISSION_GRANTED){
+//            ActivityCompat.requestPermissions(
+//                    this
+//                    , new String[] {"android.permission.OVERRIDE_WIFI_CONFIG"},
+//                    REQUEST_CODE_ASK_PERMISSIONS);
+//            return;
+//        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_PERMISSIONS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(TAG, "granttt");
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+                    Log.d(TAG, "denied");
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    private void showExplanation(String title,
+                                 String message,
+                                 final String permission,
+                                 final int permissionRequestCode) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title)
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        requestPermission(permission, permissionRequestCode);
+                    }
+                });
+        builder.create().show();
+    }
+
+    private void requestPermission(String permissionName, int permissionRequestCode) {
+        ActivityCompat.requestPermissions(this,
+                new String[]{permissionName}, permissionRequestCode);
+    }
 
     private String printWifi(){
         if (wifiManager.isWifiEnabled()){
