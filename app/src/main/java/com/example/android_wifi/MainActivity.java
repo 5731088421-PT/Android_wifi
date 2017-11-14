3166bda44fb73403fd53b8e264a9c7050d7c83e0
Panupong <panupong.not@hotmail.com>
2017-11-15 02:29:58 +0700

    Add ui mainActivity

--- app/src/main/java/com/example/android_wifi/MainActivity.java
+++ app/src/main/java/com/example/android_wifi/MainActivity.java
@@ -22,6 +22,8 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import java.lang.reflect.Method;
@@ -38,10 +40,21 @@ public class MainActivity extends AppCompatActivity {
     WifiConfiguration wifiConfig;
     Timer timer;
 
+    Button clientButton;
+    Button hotspotButton;
+    Context context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        apManager.showWritePermissionSettings(false);
+        context = getApplicationContext();
+
+        clientButton = (Button) findViewById(R.id.clientButton);
+        clientButton.setOnClickListener(buttonListenner);
+        hotspotButton = (Button) findViewById(R.id.hotspotButton);
+        hotspotButton.setOnClickListener(buttonListenner);
 
         apManager = new ApManager(this);
         wifiConfig = new WifiConfiguration();
@@ -52,9 +65,24 @@ public class MainActivity extends AppCompatActivity {
         clientMode();
     }
 
+    private View.OnClickListener buttonListenner = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (v.getId() == R.id.clientButton){
+                clientMode();
+                Intent intent = new Intent(context, ClientActivity.class);
+                startActivity(intent);
+            }
+            else if(v.getId() == R.id.hotspotButton){
+                hotspotMode();
+                Intent intent = new Intent(context, ServerActivity.class);
+                startActivity(intent);
+            }
+        }
+    };
+
 
     private void hotspotMode(){
-        apManager.showWritePermissionSettings(false);
         if(apManager.isWifiApEnabled()){
             apManager.setWifiApEnabled(null, false);
         }
